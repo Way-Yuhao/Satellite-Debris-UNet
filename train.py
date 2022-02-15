9f3f93a7c8c45342e21a2234baa50a6d6748041e
Yuhao L <36289795+Way-Yuhao@users.noreply.github.com>
2022-02-15 15:58:52 -0600

    added imports

---
+++ train.py
@@ -0,0 +1,18 @@
+__author__ = "Yuhao Liu"
+
+import os
+import os.path as p
+import torch
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+from ternausnet.models import UNet16
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
